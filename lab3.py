ac5920224c221ab39feace8446906994ce81fcd8
Bohun99 <61537909+Bohun99@users.noreply.github.com>
2020-04-08 21:01:42 +0200

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -15,3 +15,25 @@ A = [[random.randint(0,10) for x in range(4)],
 B  = [A[x][x] for x in range(4) ]
 print(A)
 print(B)
+
+#Zadanie 3
+slownik = {"ziemniaki" : "kg" , "chleb" : "sztuki" , "czekolada": "sztuki" , "jabłka": "kg" , "ketchup": "sztuki"}
+sztuki = [x for x in slownik if slownik[x] == "sztuki"]
+print(sztuki)
+
+#Zadanie 4
+
+def monotonicznosc(a):
+    if(a > 0):
+        print("Funkcja jest rosnąca")
+
+    elif (a < 0):
+        print("Funkcja jest malejąca")
+
+    else:
+        print("Funkcja jest stała ")
+
+a = input("Podaj a w funkcji y=ax+b")
+a = int(a)
+print (monotonicznosc(a))
+
