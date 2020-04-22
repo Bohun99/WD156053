1abc68afdbf48e101ff5d67ad46f3f452bd9f785
Adrian Romanik <61537909+Bohun99@users.noreply.github.com>
2020-04-22 23:07:08 +0200

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -31,3 +31,55 @@ sweter1 = swetry("bez rekawa")
 print(ubranie1.wyswietl())
 
 #ZADANIE 2
+
+
+class kwadrat:
+
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+
+    def __add__(self):
+        return self.x*2, self.y*2
+
+kw1 = Kwadrat(5)
+kw2= Kwadrat(7)
+kw3= kw1 + kw2
+print(kw3)
+
+#ZADANIE 6
+
+class Wspak:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+slowo=Wspak("mrowisko")
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+
+#ZADANIE 7
+
+
+def parzyste(dane):
+    for index in range(0, len(dane)):
+        if index % 2 == 0:
+            yield dane[index]
+napis = parzyste("Moro")
+print(next(napis))
+print(next(napis))
