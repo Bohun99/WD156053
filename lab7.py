c1e0390c59fd6bfd3887d8aa5529af26abb8dfac
Adrian Romanik <61537909+Bohun99@users.noreply.github.com>
2020-05-07 00:09:38 +0200

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -13,25 +13,20 @@ import numpy as np
 
 a= np.arange(9).reshape(3,3)
 b= np.arange(16).reshape(4,4)
-print(a)
-print("\n")
-print(b)
-print("\n")
-print(a.min(axis=0))
-print("\n")
-print(b.min(axis=0))
-print("\n")
-print(a.min(axis=1))
-print("\n")
-print(b.min(axis=1))
+print(a, "\n")
+print(b, "\n")
+print(a.min(axis=0), "\n")
+print(b.min(axis=0), "\n")
+print(a.min(axis=1), "\n")
+print(b.min(axis=1), "\n")
 
 #ZADANIE 3
 
 import numpy as np
 a = np.arange(3)
 b = np.arange(3,6)
-print(a)
-print(b)
+print(a, "\n")
+print(b, "\n")
 print(np.dot(a,b))
 
 #ZADANIE 4
@@ -42,7 +37,7 @@ print(np.dot(a,b))
 
 import numpy as np
 a1= np.arange(6).reshape(2,3)
-print(a1)
+print(a1, "\n")
 a=np.sin(a1)
 print(a)
 
@@ -50,7 +45,7 @@ print(a)
 
 import numpy as np
 b1= np.arange(6).reshape(2,3)
-print(b1)
+print(b1, "\n")
 b=np.cos(b1)
 print(b)
 
@@ -59,18 +54,14 @@ print(b)
 import numpy as np
 
 a1= np.arange(6).reshape(2,3)
-print(a1)
-print("\n")
+print(a1, "\n")
 a=np.sin(a1)
-print(a)
-print("\n")
+print(a, "\n")
 
 b1= np.arange(6).reshape(2,3)
-print(b1)
-print("\n")
+print(b1, "\n")
 b=np.cos(b1)
-print(b)
-print("\n")
+print(b, "\n")
 
 print(np.add(a, b))
 
