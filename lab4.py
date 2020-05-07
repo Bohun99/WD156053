ef18cdabde421cbd6986caf935f1dce8eeebcf06
Adrian Romanik <61537909+Bohun99@users.noreply.github.com>
2020-05-07 22:54:18 +0200

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -23,3 +23,30 @@ with open("zadanie3.txt", "w") as plik:
 with open("zadanie3.txt", "r") as plik1:
     for linia in plik1:
         print(linia, end="")
+
+        
+#ZADANIE 4
+
+class NaZakupy:
+    nazwa_produktu = ""
+    ilosc = 0
+    jednostka_miary = ""
+    cena_jed = 0
+    def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed ):
+        self.nazwa_produktu = nazwa_produktu
+        self.ilosc = ilosc
+        self.jednostka_miary = jednostka_miary
+        self.cena_jed = cena_jed
+    
+    def wyswietl_produkt(self):
+        print(f"{self.nazwa_produktu} {self.ilosc} {self.jednostka_miary} {self.cena_jed}")
+    
+    def ile_produktu(self):
+        print(f"{self.ilosc} {self.jednostka_miary}")
+    
+    def ile_kosztuje(self):
+        return self.ilosc*self.cena_jed
+produkt = NaZakupy("woda", 2, "sztuki", 1.50)
+produkt.wyswietl_produkt()
+produkt.ile_produktu()
+print(produkt.ile_kosztuje())
